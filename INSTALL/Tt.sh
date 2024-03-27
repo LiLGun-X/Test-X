1f4f6b63e49abc2101a07d58d0a76b4381348be4
LiLGun-X <91993374+LiLGun-X@users.noreply.github.com>
2024-03-27 08:03:15 +0700

    Update Tt.sh

--- INSTALL/Tt.sh
+++ INSTALL/Tt.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+fun_bar() {
+		comando[0]="$1"
+		comando[1]="$2"
+		(
+			[[ -e $HOME/fim ]] && rm $HOME/fim
+			[[ ! -d /etc/SSHPlus ]] && rm -rf /bin/menu
+			${comando[0]} >/dev/null 2>&1
+			${comando[1]} >/dev/null 2>&1
+			touch $HOME/fim
+		) >/dev/null 2>&1 &
+		tput civis
+		echo -ne "\033[1;33mกรุณารอสักครู่ \033[1;37m- \033[1;33m["
+		while true; do
+			for ((i = 0; i < 18; i++)); do
+				echo -ne "\033[1;31m#"
+				sleep 0.1s
+			done
+			[[ -e $HOME/fim ]] && rm $HOME/fim && break
+			echo -e "\033[1;33m]"
+			sleep 1s
+			tput cuu1
+			tput dl1
+			echo -ne "\033[1;33mกรุณารอสักครู่ \033[1;37m- \033[1;33m["
+		done
+		echo -e "\033[1;33m]\033[1;37m -\033[1;32m สำเร็จ !\033[1;37m"
+		tput cnorm
+	}
+
 inst_sqd() {
 		if netstat -nltp | grep 'squid' 1>/dev/null 2>/dev/null; then
 			echo -e "\E[41;1;37m            REMOVER SQUID PROXY              \E[0m"
@@ -267,4 +295,4 @@ inst_sqd() {
 		esac
 	}
 	
-fun_squid
+
